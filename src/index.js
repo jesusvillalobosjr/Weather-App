8795db4ac2d7ebe73909386ded54d6a555ee90f2
Jesus Villalobos Jr <jesus.villalobosjr@gmail.com>
2024-04-25 14:47:00 -1000

    Add styling to app for visuals

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// async function getWeather(location) {
+//     const url = `http://api.weatherapi.com/v1/forecast.json?key=420fa5346009407f944213502241204&q=${location}&days=5`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+// }
+
+
+
+// getWeather('London');
